f0d2a5433c8bac3893bd7857f4bdaecb4845aed7
ArtemVahrin <artemvahrin6@gmail.com>
2025-12-27 17:03:03 +0300

    Initial Commit

---
+++ MemorzieGame/MemorzieGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorzieGameApp.swift
+//  MemorzieGame
+//
+//  Created by Артём on 27.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorzieGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
